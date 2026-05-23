d51063c0c07966c86d5e2edf34cf88e10e89acb0
ArafatSany7 <human.sany7@gmail.com>
2026-05-23 14:57:49 +0600

    Depolyment success

--- dist/config/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const _default: {
-    port: string | number;
-    database_url: string | undefined;
-    jwt_secret: string | undefined;
-    jwt_expires_in: string | undefined;
-    bcrypt_salt_rounds: number;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
